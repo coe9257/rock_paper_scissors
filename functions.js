5b57de1d8051bb50254ff4c49b3cf1f49b91f549
William <coe9257@gmail.com>
2023-11-11 17:52:28 -0800

    initial commit

---
+++ functions.js
@@ -0,0 +1,7 @@
+export function start_round() {
+
+    console.log("working");
+    let heading_text = document.querySelector(".round_title");
+        heading_text.textContent = "ROCK PAPER SCISSORS";
+
+}
---
+++ index.js
@@ -0,0 +1,8 @@
+import { start_round } from './functions.js';
+
+const element_object = {
+
+}
+
+document.querySelector(".button").addEventListener("click", (start_round));
+
